64ecdfb94adb9b54fe2f44f5e2ea65f334314050
baaaam771 <jeongiun@naver.com>
2022-03-10 19:30:59 +0900

    posting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import { Component } from 'react';
 import api from './api';
+import PostView from './Components/PostView';
+
 
 class App extends Component {
   constructor(props) {
@@ -9,8 +11,24 @@ class App extends Component {
     this.state = {
       title: '',
       content: '',
+      results: [],
     }
   }
+
+
+  componentDidMount() {
+    this.getPosts()
+  }
+
+  async getPosts() {
+    const _results = await api.getAllPosts()
+    this.setState({results: _results.data})
+    // _results.data 아무것도 없음
+    console.log(_results)
+  }
+
+
+
 
   handlingChange = (event) => {
     this.setState({[event.target.name]: event.target.value})
@@ -19,14 +37,14 @@ class App extends Component {
   handlingSubmit = async (event) => {
     event.preventDefault()// event 기능 막기 (새로고침 방지)
     let result = await api.createPost({title: this.state.title, content: this.state.content})
-    console.log("완료됨!", result)
+    console.log("완료됨!", result.data)
   }
 
   render() {
     return (
       <div className="App">
         <div className="PostingSection">
-          <h2>대나무 숲 글 작성하기</h2>
+          <h2>글 작성하기</h2>
           <form onSubmit = {this.handlingSubmit}>
           <input 
             name = "title"
@@ -45,7 +63,12 @@ class App extends Component {
           </form>
         </div>
         <div className="ViewSection">
-
+          {
+            this.state.results.map((post) =>
+            <PostView key = {post.id} title = {post.title} content = {post.content}/>
+            )
+          }
+          
         </div>
       </div>
     );
